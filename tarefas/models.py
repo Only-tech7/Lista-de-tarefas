75fd8b55823f6452b47255e511eebd7232604e54
Only-tech7 <theuzztrapper@gmail.com>
2026-04-10 17:08:45 -0300

    primeira versão do projeto

---
+++ tarefas/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+# Create your models here.
+class Tarefa(models.Model):
+    titulo = models.CharField(max_length=255)
+concluida = models.BooleanField(default=False)
+criada_em = models.DateTimeField(auto_now_add=True)
+
+def __str__(self):
+    return self.titulo
