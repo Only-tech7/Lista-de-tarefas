4e146f8f1a160d92dfa29a9f6bf1591013ed4a4e
Only-tech7 <theuzztrapper@gmail.com>
2026-04-14 16:03:54 -0300

    Adiciona gitignore e limpa arquivos desnecessários

--- tarefas/models.py
+++ tarefas/models.py
@@ -1,9 +1,11 @@
+#models.py define os campos da tabela do banco de dados, ou seja, a estrutura da tabela. Ele é responsável por criar a tabela no banco de dados e definir os tipos de dados para cada campo. O models.py é onde você define as classes que representam as tabelas do banco de dados e os campos dessas tabelas. Cada classe representa uma tabela e cada atributo da classe representa um campo da tabela. O Django usa essas classes para criar as tabelas no banco de dados e para manipular os dados dessas tabelas.
 from django.db import models
 # Create your models here.
 class Tarefa(models.Model):
     titulo = models.CharField(max_length=255)
-concluida = models.BooleanField(default=False)
-criada_em = models.DateTimeField(auto_now_add=True)
+    concluida = models.BooleanField(default=False)
+    criada_em = models.DateTimeField(auto_now_add=True)
 
 def __str__(self):
     return self.titulo
+
--- tarefas/serializers.py
+++ tarefas/serializers.py
@@ -1,3 +1,4 @@
+#define quais campos da tabela do banco de dados serão usados para criar a API. O serializers.py é responsável por converter os dados do modelo em um formato que possa ser facilmente enviado e recebido pela API, como JSON ou XML. Ele também é responsável por validar os dados recebidos pela API antes de salvá-los no banco de dados. O serializers.py é onde você define as classes que representam os dados que serão enviados e recebidos pela API, e os campos dessas classes correspondem aos campos do modelo definido em models.py.
 from rest_framework import serializers
 from .models import Tarefa
 
